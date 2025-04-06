2998a083d7b239364f3d4d90c0c0d708f09d6c95
Turge08 <Turge08@gmail.com>
2025-04-06 15:30:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ fetch('components.json')
     .then(data => {
         const componentList = document.getElementById('component-list');
         data.forEach(component => {
+            const repoUrl = `https://github.com/${component.repo}`;
             const row = document.createElement('tr');
             row.innerHTML = `
                 <td data-label="Name">${component.name}</td>
                 <td data-label="Description">${component.description || 'No description provided'}</td>
-                <td data-label="Repository"><a href="${component.repo}" target="_blank">GitHub</a></td>
+                <td data-label="Repository"><a href="${repoUrl}" target="_blank">GitHub</a></td>
             `;
             componentList.appendChild(row);
         });
